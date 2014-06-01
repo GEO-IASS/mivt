529d56479c2e3aecf33c7b92f880c5240b1b10ac
Adrian Dalca <adalca@mit.edu>
2014-06-01 12:51:35 -0400

    updated viewPatchMatches description

--- src/@patchlib/readme.md
+++ src/@patchlib/readme.md
@@ -5,16 +5,16 @@ A library for working with patches, currently very much in development.
 
 Viewing Functions
 -----------------
-- `patchlib.view.patchesInImage`: visualize 2D patches in an image
-- `patchlib.view.patchMatches` display 2D patches matching an original patch
-- `patchlib.view.patches2D`: show 2D patches in a subplot grid
+- `patchlib.view.patchesInImage` visualize 2D patches in an image
+- `patchlib.view.patchMatches2D` display 2D patches matching an original patch
+- `patchlib.view.patches2D` show 2D patches in a subplot grid
 
 Test Functions
 --------------
-- `patchlib.test.viewPatchesInImage`: test view.patchesInImage
-- `patchlib.test.viewPatchMatches2D`: test view.patchMatches2D
+- `patchlib.test.viewPatchesInImage` test view.patchesInImage
+- `patchlib.test.viewPatchMatches2D` test view.patchMatches2D
 
 Helper Functions
 ----------------
-- `patchlib.guessPatchSize`: guess the size of a patch from nVoxels
-- `patchlib.patchCenterDist`: compute the distance to the center of a patch
+- `patchlib.guessPatchSize` guess the size of a patch from nVoxels
+- `patchlib.patchCenterDist` compute the distance to the center of a patch
--- src/@patchlib/testViewPatchMatches2D.m
+++ src/@patchlib/testViewPatchMatches2D.m
@@ -9,13 +9,13 @@ function testViewPatchMatches2D()
     [origPatch, matches, seg] = simgroup(patchSize, nMatches);
     
     % test simple original and matches
-    patchlib.viewPatchMatches(patchSize, origPatch, matches);
+    patchlib.viewPatchMatches2D(patchSize, origPatch, matches);
     
     % test simple original and matches, with a different intensity scale, and segmentations
-    patchlib.viewPatchMatches(patchSize, origPatch*100, matches*100, seg, 'caxis', {[0 100], [0, 100], [0, 1]});
+    patchlib.viewPatchMatches2D(patchSize, origPatch*100, matches*100, seg, 'caxis', {[0 100], [0, 100], [0, 1]});
     
     % test original, matches, label equivalent and a second label 
-    patchlib.viewPatchMatches(patchSize, origPatch, matches, seg, seg .* (rand(size(seg)) < 0.5));
+    patchlib.viewPatchMatches2D(patchSize, origPatch, matches, seg, seg .* (rand(size(seg)) < 0.5));
 
     % test several original patches with matching groups and segmentations
     origPatches = zeros(nReps, prod(patchSize));
@@ -24,7 +24,7 @@ function testViewPatchMatches2D()
     for r = 1:nReps
         [origPatches(r, :), matchescell{r}, segcell{r}] = simgroup(patchSize, nMatches);
     end
-    patchlib.viewPatchMatches(patchSize, origPatches, matchescell, segcell);
+    patchlib.viewPatchMatches2D(patchSize, origPatches, matchescell, segcell);
     
 end
 
