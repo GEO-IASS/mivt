8ab66e63d6d0a6bd5c91c7afcf228017b2242ec2
Adrian Dalca <adalca@mit.edu>
2014-06-16 16:02:11 -0400

    add multiple references support to lib2patches.m

--- src/@patchlib/lib2patches.m
+++ src/@patchlib/lib2patches.m
@@ -5,32 +5,34 @@ function patches = lib2patches(lib, pIdx, varargin)
 %   lib2patches(lib, pIdx, 'cell')
 %   lib2patches(lib, pIdx, patchSize, 'cell')
 %
+% with a cell of libs clibs:
+%   lib2patches(clib, pIdx, lIdx, ...)
+%
 %
 %   lib - N x V
 %   pIdx - M x K
 %   patches - M x V x K or cell of {M x K} patchSize patches.
-%
 
-    if nargin == 2
-        patchSize = patchlib.guessPatchSize(size(lib, 2));
+    % if cell library 
+    if iscell(lib)
+        narginchk(3, inf);
+        lIdx = varargin{1};
+        varargin = varargin(2:end);
+    else
+        lib = {lib};
+        lIdx = ones(size(pIdx));
     end
-    docell = '';
-    
-    if nargin >= 3
-        if isnumeric(varargin{1})
-            patchSize = varargin{1};
-        else
-            docell = varargin{1};
-            patchSize = patchlib.guessPatchSize(size(lib, 2));
-        end
-    end
-       
-    if nargin == 4
-        docell = varargin{2};
-    end    
+
+    % check inputs
+    [patchSize, docell] = parseinputs(size(lib{1}, 2), varargin{:});
     K = size(pIdx, 2);
     
-    tmppatches = lib(pIdx(:), :);
+    % initiate patches
+    tmppatches = zeros([numel(pIdx), size(lib{1}, 2)]);
+    for i = 1:numel(lib)
+        libmap = lIdx(:) == i;
+        tmppatches(libmap, :) = lib{i}(pIdx(libmap), :);
+    end
     
     % reshape to [M x K x V]
     tmppatches = reshape(tmppatches, [size(pIdx), prod(patchSize)]);
@@ -49,4 +51,29 @@ function patches = lib2patches(lib, pIdx, varargin)
         % reshape to [M x V x K]
         patches = permute(tmppatches, [1, 3, 2]);    
     end
+    
+end
+
+function [patchSize, docell] = parseinputs(V, varargin)
+    narginchk(1, 2)
+    
+    if nargin == 1
+        patchSize = patchlib.guessPatchSize(V);
+    end
+    docell = '';
+    
+    if nargin >= 1
+        if isnumeric(varargin{1})
+            patchSize = varargin{1};
+        else
+            docell = varargin{1};
+            patchSize = patchlib.guessPatchSize(V);
+        end
+    end
+       
+    if nargin == 3
+        docell = varargin{2};
+    end    
+end
+    
     
