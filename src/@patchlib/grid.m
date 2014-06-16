1a1d36ad309c52b62c849a03c0241cfcdc7e2dbb
Adrian Dalca <adalca@mit.edu>
2014-06-16 15:54:39 -0400

    improving documentation formatting.

--- src/@patchlib/grid.m
+++ src/@patchlib/grid.m
@@ -1,42 +1,39 @@
 function [idx, newVolSize, nPatches, overlap] = grid(volSize, patchSize, varargin)
 % GRID grid of patch starting points for n-d volume
-%   idx = grid(volSize, patchSize) computed the grid of patches that fit into volSize given a 
-%       particular patchSize, and return the indexes of the top-left voxel of each patch. patchSize
-%       should be the same length as volSize. By default, the patches are assumed to be sliding -
-%       that is, patches will have an overlap of (patchSize - 1). See below for specifying patch
-%       overlaps.
-%
-%       The index is in the given volume. If the volume gets cropped as part of the function and you
-%       want a linear indexing into the new volume size, use 
-%       >> newidx = ind2ind(newVolSize, volSize, idx); 
-%       newVolSize can be passed by the current function, see below.
-%
-%   idx = grid(volSize, patchSize, patchOverlap) specifiy the amount of patch overlap. The volume
-%       will be cropped to the maximum size that fits the patch grid. For example, a [6x6] volume
-%       with a patchsize of [3x3] and overlap of 1 will be cropped to [5x5] volume. Overlap should
-%       be < patchSize. patchOverlap should be a scalar (applied to all dim) or the same length as
-%       patchSize.
-%
-%       To get spacing between non-overlapping patches, enter negative overlaps.
-%
-%   idx = grid(volSize, patchSize, kind) allows for pre-specified kind of overlaps:
-%       like 'sliding', 'discrete', or 'mrf'. see patchlib.overlapkind for details of the supported
-%       overlap kinds. If not specified (i.e. function has only 2 inputs), default overlap is
-%       'sliding'.
-%
-%   idx = grid(..., patchOverlap/kind, startSub) - start the indexing at a particular location [1 x
-%       nDims]. This essentially means that the volume will be cropped starting at that location.
-%       e.g. if startSub is [2, 2], then only vol(2:end, 2:end) will be included.
-%
-%   sub = grid(..., patchOverlap/kind, 'sub') return n-D subscripts instead of linear index. sub 
-%       will be a 1 x nDim cell. This is equivalent to [sub{:}] = ind2sub(volSize, idx), but is done
-%       faster inside this function.
-%
-%   [..., newVolSize, nPatches, overlap] = grid(...) returns the size of the cropped volume, the
-%       number of patches in each direction, and the size of the overlap. The latter is useful is
-%       the 'kind' input was used.
-%
-%   TODO: could speed up for the special case of 2D or 3D?
+%     idx = grid(volSize, patchSize) computed the grid of patches that fit into volSize given a 
+%     particular patchSize, and return the indexes of the top-left voxel of each patch. patchSize
+%     should be the same length as volSize. By default, the patches are assumed to be sliding - that
+%     is, patches will have an overlap of (patchSize - 1). See below for specifying patch overlaps.
+% 
+%     The index is in the given volume. If the volume gets cropped as part of the function and you
+%     want a linear indexing into the new volume size, use >> newidx = ind2ind(newVolSize, volSize,
+%     idx); newVolSize can be passed by the current function, see below.
+% 
+%     idx = grid(volSize, patchSize, patchOverlap) specifiy the amount of patch overlap. The volume
+%     will be cropped to the maximum size that fits the patch grid. For example, a [6x6] volume with
+%     a patchsize of [3x3] and overlap of 1 will be cropped to [5x5] volume. Overlap should be <
+%     patchSize. patchOverlap should be a scalar (applied to all dim) or the same length as
+%     patchSize.
+% 
+%     To get spacing between non-overlapping patches, enter negative overlaps.
+% 
+%     idx = grid(volSize, patchSize, kind) allows for pre-specified kind of overlaps: like
+%     'sliding', 'discrete', or 'mrf'. see patchlib.overlapkind for details of the supported overlap
+%     kinds. If not specified (i.e. function has only 2 inputs), default overlap is 'sliding'.
+% 
+%     idx = grid(..., patchOverlap/kind, startSub) - start the indexing at a particular location [1
+%     x nDims]. This essentially means that the volume will be cropped starting at that location.
+%     e.g. if startSub is [2, 2], then only vol(2:end, 2:end) will be included.
+% 
+%     sub = grid(..., patchOverlap/kind, 'sub') return n-D subscripts instead of linear index. sub
+%     will be a 1 x nDim cell. This is equivalent to [sub{:}] = ind2sub(volSize, idx), but is done
+%     faster inside this function.
+% 
+%     [..., newVolSize, nPatches, overlap] = grid(...) returns the size of the cropped volume, the
+%     number of patches in each direction, and the size of the overlap. The latter is useful is the
+%     'kind' input was used.
+% 
+%     TODO: could speed up for the special case of 2D or 3D?
 %
 % See also: overlapkind, patchcount
 %
