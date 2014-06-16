9c9f19177ea80765457b267c738a53db8d3504c0
Adrian Dalca <adalca@mit.edu>
2014-06-16 16:00:47 -0400

    rename and update grid2volSize.m

--- src/@patchlib/grid2volSize.m
+++ src/@patchlib/grid2volSize.m
@@ -1,13 +1,13 @@
-function volSize = nPatches2volSize(nPatches, patchSize, varargin)
+function volSize = grid2volSize(gridSize, patchSize, varargin)
 % NPATCHES2VOLSIZE volume size from number of patches
-%   volSize = nPatches2volSize(nPatches, patchSize) compute the size of a volume of patches given
+%   volSize = grid2volSize(gridSize, patchSize) compute the size of a volume of patches given
 %       the patch size and number of patches. This assumes sliding patches (i.e. patch overlap of
 %       patchSize - 1)
 %
-%   volSize = nPatches2volSize(nPatches, patchSize, patchOverlap) allows for the specification of
+%   volSize = grid2volSize(gridSize, patchSize, patchOverlap) allows for the specification of
 %       amount of patch overlap.
 %
-%   volSize = nPatches2volSize(nPatches, patchSize, kind) allows for pre-specified kind of overlaps:
+%   volSize = grid2volSize(gridSize, patchSize, kind) allows for pre-specified kind of overlaps:
 %       like 'sliding', 'discrete', or 'mrf'. see patchlib.overlapkind for details of the supported
 %       overlap kinds. If not specified (i.e. function has only 2 inputs), default overlap is
 %       'sliding'.
@@ -26,5 +26,5 @@ function volSize = nPatches2volSize(nPatches, patchSize, varargin)
         end
     end
     
-    volSize  = (patchSize - patchOverlap) .* nPatches + patchOverlap;
+    volSize  = (patchSize - patchOverlap) .* gridSize + patchOverlap;
     
