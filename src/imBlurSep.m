b1e109d32479aa4afd93f14b626b5758c46440b6
Adrian Dalca <adalca@mit.edu>
2014-12-29 18:18:44 -0500

    update imBlurSep

--- src/imBlurSep.m
+++ src/imBlurSep.m
@@ -3,12 +3,9 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
 % 	vol the volume (nDims)
 % 	window should be (nDims x 1)
 % 	sigma is a scalar in mm
-% 	voxDims the dimensions of the voxels in mm (nDims x 1)
-%	padType - 'nn' for nearest neighbour padding, any other string for no padding
-%
-% TODO: rename to volblur?
-%   - allow no sigma param (and estimate it say with 3x stdev)
-%   
+% 	voxDims [optional] the dimensions of the voxels in mm (nDims x 1)
+%	padType [optional] - 'nn' for nearest neighbour padding, any other string for no padding.
+%       default: nn
 %
 % Contact: adalca@mit.edu
 
@@ -16,8 +13,12 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
         sigma = ones(1, ndims(vol)) * sigma;
     end
 
-    if nargin == 4
-        padType = 'zeros';
+    if nargin <= 3
+        voxDims = ones(size(window));
+    end
+    
+    if nargin <= 4
+        padType = 'nn';
     end
     assert(all(mod(window, 2) == 1));
     
@@ -26,7 +27,9 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
     onesVec = ones(1, ndims(vol));
     
     for i = 1:ndims(vol)
-        filter = fspecial('gaussian', [1, window(i)], sigma(i) ./ voxDims(i));
+%         filterchk = fspecial('gaussian', [1, window(i)], sigma(i) ./ voxDims(i));
+        filter = gaussianfilter([1, window(i)], sigma(i) ./ voxDims(i));
+%         assert(all(filter == filterchk));        
         reshapeVec = onesVec;
         reshapeVec(i) = window(i);
         filt{i} = reshape(filter, reshapeVec);
@@ -46,4 +49,21 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
 
     assert(all(size(filtVol) == volSize));
 
+end
+
+function h = gaussianfilter(p2, p3)
+
+    siz   = (p2-1)/2;
+     std   = p3;
+     
+     [x,y] = meshgrid(-siz(2):siz(2),-siz(1):siz(1));
+     arg   = -(x.*x + y.*y)/(2*std*std);
+
+     h     = exp(arg);
+     h(h<eps*max(h(:))) = 0;
+
+     sumh = sum(h(:));
+     if sumh ~= 0,
+       h  = h/sumh;
+     end;
 end
