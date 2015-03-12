4e27c07c706d490c66e0dcbcb19d207bdbd9b0cc
Adrian Dalca <adalca@mit.edu>
2015-03-12 16:24:00 -0400

    improved volblur and volfilt code

--- src/imBlurSep.m
+++ src/imBlurSep.m
@@ -9,6 +9,7 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
 %
 % Contact: adalca@mit.edu
 
+
     % input parsing
     narginchk(3, 5);
     if isscalar(sigma)
@@ -23,50 +24,7 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
         padType = 'nn';
     end
     assert(all(mod(window, 2) == 1));
-    
-    % create filters
-    filt = cell(1, ndims(vol));
-    onesVec = ones(1, ndims(vol));
-    for i = 1:ndims(vol)
-        filter = gaussianfilter([1, window(i)], sigma(i) ./ voxDims(i));
-        % filterchk = fspecial('gaussian', [1, window(i)], sigma(i) ./ voxDims(i));
-        % assert(all(filter == filterchk));        
-        reshapeVec = onesVec;
-        reshapeVec(i) = window(i);
-        filt{i} = reshape(filter, reshapeVec);
-    end
 
-    % filter volume.
-    switch padType
-        
-        case 'nn' 
-            % if using nearest neighbour padding
-            % pad via replicates and crop back after doing the filtering.
-            inputVol = padarray(vol, (window - 1)/2, 'replicate', 'both');
-            filtVol = imfilterSep(inputVol, filt{:}, 'valid');
-                
-        otherwise
-            filtVol = imfilterSep(vol, filt{:});
-    end
-    
-    assert(all(size(filtVol) == size(vol)));
-end
-
-function h = gaussianfilter(window, sigma)
-% fast implementation of gaussian filter
-% TODO: check: this works in 3D?
-
-    siz = (window-1)/2;
-    std = sigma;
-
-    [x,y] = meshgrid(-siz(2):siz(2),-siz(1):siz(1));
-    arg = -(x.*x + y.*y)/(2*std*std);
-
-    h     = exp(arg);
-    h(h<eps*max(h(:))) = 0;
-
-    sumh = sum(h(:));
-    if sumh ~= 0,
-        h  = h/sumh;
-    end;
+    warning('imBlurSep is being phased out. use volblur');
+    [filtVol, filt]  = volblur(vol, sigma, windows, 'voxDims', voxDims, 'padType', padType);
 end
--- src/imfilterSep.m
+++ src/imfilterSep.m
@@ -12,43 +12,8 @@ function fim = imfilterSep(im, varargin)
 %
 %   Author: Adrian V. Dalca, http://www.mit.edu/~adalca/
 
-    imDims = ndims(im);
-    
-    if ischar(varargin{end})
-        shape = varargin{end};
-        inputFilters = varargin(1:numel(varargin) - 1);
-    else
-        shape = 'same';
-        inputFilters = varargin;
-    end
-        
-    
-    
-    % if only one separable filter provided, assume it is to be used in
-    % each dimension.
-    onesVec = ones(1, ndims(im));
-    filters = cell(1, imDims);
-    if numel(inputFilters) == 1 && imDims > 1
-        assert(isvector(inputFilters{1}), 'second input must be a vector');
-        for i = 1:ndims(im)
-            reshapeVec = onesVec;
-            reshapeVec(i) = numel(inputFilters{1});
-            filters{i} = reshape(inputFilters{1}, reshapeVec);
-        end     
-        
-    % otherwise assume we are provided with the right number of filters in
-    % the right order. 
-    else
-        assert(numel(inputFilters) == imDims);
-        filters = inputFilters;
-    end
+    warning('imfilterSep is being phased out. use volfilt');
 
-    
-    
-    % perform the filtering
-    fim = im;
-    for i = 1:imDims
-        fim = convn(fim, filters{i}, shape);
-    end     
+    fim = volFilterSet(im, varargin{:});
 end 
 
