0a76653859838b72ae82cf66225f9d2349bbc95b
Adrian Dalca <adalca@mit.edu>
2015-05-28 14:24:42 -0400

    slight update to imBlurSep

--- src/imBlurSep.m
+++ src/imBlurSep.m
@@ -25,6 +25,6 @@ function [filtVol, filt]  = imBlurSep(vol, window, sigma, voxDims, padType)
     end
     assert(all(mod(window, 2) == 1));
 
-    warning('imBlurSep is being phased out. use volblur');
+    warning('imBlurSep is being phased out. use volblur()');
     [filtVol, filt]  = volblur(vol, sigma, window, 'voxDims', voxDims, 'padType', padType);
 end
